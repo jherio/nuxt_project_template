02d2234a1b59e0fd8e11aac8db6c0fe63505b5da
jherio <robinson.jhe@gmail.com>
2024-08-27 13:16:35 +0100

    Added components/atoms/ReusableButton.vue

---
+++ components/atoms/ReusableButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button
+    class="bg-primary text-white font-bold py-2 px-4 rounded hover:bg-secondary transition duration-300"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineEmits(['click'])
+</script>
